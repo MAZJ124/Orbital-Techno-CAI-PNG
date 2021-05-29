89926223463143763c25a4257b9345f8882cb333
itsleeqian <itsleeqian@gmail.com>
2021-05-29 22:12:35 +0800

    browsing page somewhat completed

---
+++ lib/browse.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class Browse extends StatefulWidget {
+  const Browse({Key key}) : super(key: key);
+
+  @override
+  _BrowseState createState() => _BrowseState();
+}
+
+class _BrowseState extends State<Browse> {
+
+  List<String> locations = [
+    'TechnoEdge',
+    'The Deck',
+    'E2 Canteen',
+    'Yusof Ishak House Canteen',
+    'Central Library'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+        'NUSpots',
+        style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        minimum: EdgeInsets.all(15.0),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black26),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                        'Study',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 5.0),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black26),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Eat',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 5.0),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black26),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'All',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: locations.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      onTap: () {},
+                      title: Text(locations[index]),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                    ),
+                  );
+                }
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
