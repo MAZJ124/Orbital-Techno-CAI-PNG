5c2ad050a49f41a0dc8df56346798f0862c7be90
MAZJ124 <yuangazi@hotmail.com>
2021-06-23 19:20:00 +0800

    Implemented search function with first letter

--- lib/browse_page.dart
+++ lib/browse_page.dart
@@ -9,182 +9,220 @@ class Browse extends StatefulWidget {
 }
 
 class _BrowseState extends State<Browse> {
-
+  //used to access locations from firestore database
   final fireStore = FirebaseFirestore.instance;
 
-  List<String> locations = [
-    'TechnoEdge',
-    'The Deck',
-    'E2 Canteen',
-    'Yusof Ishak House Canteen',
-    'Central Library'
-  ];
+  TextEditingController _searchController = TextEditingController();
+  List _allResults = [];
+  List _resultsList = [];
+  Future resultsLoaded;
 
   //dropdown boxes for the different categories
   bool firstValue = false;
   bool secValue = false;
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    resultsLoaded = getLocations();
+  }
+
+  _onSearchChanged() {
+    searchResultsList();
+  }
+
+  searchResultsList() {
+    var showResults = [];
+
+    if (_searchController.text != "") {
+      for (var result in _allResults){
+        var title = result['name'];
+        if(title.contains(_searchController.text.toLowerCase())) {
+          showResults.add(result);
+        }
+      }
+    }
+    else {
+      showResults = List.from(_allResults);
+    }
+    setState(() {
+      _resultsList = showResults;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  getLocations() async {
+    var data = await fireStore.collection('locations').get();
+    setState(() {
+      _allResults = data.docs;
+    });
+    searchResultsList();
+    return 'done';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-        'NUSpots',
-        style: TextStyle(
-          fontSize: 25.0,
-          fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          title: Text(
+            'NUSpots',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.green,
+          centerTitle: true,
         ),
-      ),
-        backgroundColor: Colors.green,
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        minimum: EdgeInsets.all(15.0),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.black26),
+        body: SafeArea(
+          minimum: EdgeInsets.all(15.0),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _searchController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    hintText: ' Search',
+                    suffixIcon: Icon(
+                      Icons.search,
                     ),
-                    onPressed: () {},
-                    child: Text(
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black26),
+                      ),
+                      onPressed: () {},
+                      child: Text(
                         'Study',
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        color: Colors.white,
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(width: 5.0),
-                Expanded(
-                  child: TextButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.black26),
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Eat',
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        color: Colors.white,
+                  SizedBox(width: 5.0),
+                  Expanded(
+                    child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black26),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Eat',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(width: 5.0),
-                Expanded(
-                  child: TextButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.green),
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'All',
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        color: Colors.white,
+                  SizedBox(width: 5.0),
+                  Expanded(
+                    child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'All',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 15.0),
-            //dropdown of categories
-            DropdownButton(
-              items: [
-                DropdownMenuItem(
-                  child: Row(
-                    children: <Widget>[
-                      Checkbox(
-                        value: this.firstValue,
-                        onChanged: (bool value) {
-                          setState(() {
-                            this.firstValue = value;
-                          });
-                        },
-                      ),
-                      Text('First'),
-                    ],
+                ],
+              ),
+              SizedBox(height: 15.0),
+              //dropdown of categories
+              DropdownButton(
+                items: [
+                  DropdownMenuItem(
+                    child: Row(
+                      children: <Widget>[
+                        Checkbox(
+                          value: this.firstValue,
+                          onChanged: (bool value) {
+                            setState(() {
+                              this.firstValue = value;
+                            });
+                          },
+                        ),
+                        Text('First'),
+                      ],
+                    ),
                   ),
-                ),
-                DropdownMenuItem(
-                  child: Row(
-                    children: <Widget>[
-                      Checkbox(
-                        value: this.secValue,
-                        onChanged: (bool value) {
-                          setState(() {
-                            this.secValue = value;
-                          });
-                        },
-                      ),
-                      Text('Second'),
-                    ],
-                  ),
-                )
-              ],
-              onChanged: (value) {
-              },
-              hint: Text('Select tags/categories'),
-            ),
-            Expanded(
-              // child: ListView.builder(
-              //   itemCount: locations.length,
-              //   itemBuilder: (context, index) {
-              //     return Card(
-              //       child: ListTile(
-              //         onTap: () {},
-              //         title: Text(locations[index]),
-              //         trailing: Icon(Icons.keyboard_arrow_right),
-              //       ),
-              //     );
-              //   }
-              // ),
-              child: StreamBuilder(
-                stream: fireStore.collection('locations').snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
+                  DropdownMenuItem(
+                    child: Row(
+                      children: <Widget>[
+                        Checkbox(
+                          value: this.secValue,
+                          onChanged: (bool value) {
+                            setState(() {
+                              this.secValue = value;
+                            });
+                          },
+                        ),
+                        Text('Second'),
+                      ],
+                    ),
+                  )
+                ],
+                onChanged: (value) {},
+                hint: Text('Select tags/categories'),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _resultsList.length,
+                  itemBuilder: (context, index){
+                    //add item into final list view
                     return Center(
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.lightBlueAccent,
+                      child: MaterialButton(
+                        color: Colors.black12,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                        onPressed: (){},
+                        child: SizedBox(
+                          child: Text(_resultsList[index]['name']),
+                          width: double.infinity,
+                        ),
                       ),
                     );
-                  }
-
-                  return ListView(
-                    children: snapshot.data.docs.map<Widget>((doc){
-                      return Center(
-                        child: MaterialButton(
-                          color: Colors.black12,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                          ),
-                          onPressed: (){},
-                          // width: MediaQuery.of(context).size.width / 1.2,
-                          // height: MediaQuery.of(context).size.height/ 6,
-                          child: SizedBox(
-                            child: Text(doc['name']),
-                            width: double.infinity,
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  );
-                },
-
+                  },
+                ),
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            ],
+          ),
+        ));
   }
 }
