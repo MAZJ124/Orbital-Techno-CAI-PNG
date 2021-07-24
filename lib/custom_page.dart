e9dca4c459feee61c8977d792663be7dc30630c6
itsleeqian <itsleeqian@gmail.com>
2021-07-24 15:48:44 +0800

    add new location functional

--- lib/custom_page.dart
+++ lib/custom_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_tags/flutter_tags.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:nus_spots/browse_page.dart';
 import 'globals.dart';
 
 class Custom extends StatefulWidget {
@@ -31,7 +33,26 @@ class _CustomState extends State<Custom> {
   String imgURL = '';
   String category = '';
   String error = '';
+  double currLat;
+  double currLong;
 
+  addLocation() {
+    var randomDoc = locationsRef.doc();
+
+    locationsRef
+        .doc(randomDoc.id)
+        .set(
+      {
+        "category": category,
+        "image": imgURL,
+        "lat": currLat,
+        "lng": currLong,
+        "locationID": randomDoc.id,
+        "name": locName,
+        "tags": currentTags
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,115 +68,147 @@ class _CustomState extends State<Custom> {
         backgroundColor: Colors.green,
         centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              //dropdown box for category
-              SizedBox(height: 20.0),
-              TextFormField(
-                  //validator: (val) => val.length == 1 ? 'Enter a valid location name' : null,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                //dropdown box for category
+                SizedBox(height: 20.0),
+                TextFormField(
+                    //validator: (val) => val.length == 1 ? 'Enter a valid location name' : null,
+                    onChanged: (val) {
+                      setState(() => locName = val);
+                    },
+                    decoration: new InputDecoration(
+                        hintText: 'Enter location name'
+                    )
+                ),
+                SizedBox(height: 20.0),
+                DropdownButtonFormField(
+                    items: categories.map((cat){
+                      return DropdownMenuItem(
+                        value: cat,
+                        child: Text('$cat'),
+                      );
+                    }).toList(),
                   onChanged: (val) {
-                    setState(() => locName = val);
+                    setState(() => category = val);
                   },
-                  decoration: new InputDecoration(
-                      hintText: 'Enter location name'
-                  )
-              ),
-              SizedBox(height: 20.0),
-              DropdownButtonFormField(
-                  items: categories.map((cat){
+                  decoration:  new InputDecoration(
+                      hintText: 'Choose category'
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                DropdownButtonFormField(
+                  items: allTags.map((tag){
+                    var index = allTags.indexOf(tag);
                     return DropdownMenuItem(
-                      value: cat,
-                      child: Text('$cat'),
+                      child: Row(
+                        children: <Widget>[
+                          StatefulBuilder(
+                            builder: (context, setState) => Checkbox(
+                                //value: tagValuesMap[index],
+                                value: tagValues[index],
+                                onChanged: (bool value) {
+                                  setState(() {
+                                    tagValues[index] = value;
+                                    if (tagValues[index] == true) {
+                                      currentTags.add(allTags[index]);
+                                    } else {
+                                      currentTags.removeWhere(
+                                          (item) => item == allTags[index]
+                                      );
+                                    }
+                                  });
+                                },
+                            ),
+                          ),
+                          Text('$tag'),
+                        ],
+                      ),
                     );
                   }).toList(),
-                onChanged: (val) {
-                  setState(() => category = val);
-                },
-                decoration:  new InputDecoration(
-                    hintText: 'Choose category'
-                ),
-              ),
-              SizedBox(height: 20.0),
-              DropdownButtonFormField(
-                items: allTags.map((tag){
-                  var index = allTags.indexOf(tag);
-                  return DropdownMenuItem(
-                    child: Row(
-                      children: <Widget>[
-                        StatefulBuilder(
-                          builder: (context, setState) => Checkbox(
-                              //value: tagValuesMap[index],
-                              value: tagValues[index],
-                              onChanged: (bool value) {
-                                setState(() {
-                                  tagValues[index] = value;
-                                  if (tagValues[index] == true) {
-                                    currentTags.add(allTags[index]);
-                                  } else {
-                                    currentTags.removeWhere(
-                                        (item) => item == allTags[index]
-                                    );
-                                  }
-                                });
-                              },
-                          ),
-                        ),
-                        Text('$tag'),
-                      ],
-                    ),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                },
-                decoration:  new InputDecoration(
-                    hintText: 'Choose tags'
-                ),
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                  onChanged: (val) {
-                    setState(() => imgURL = val);
+                  onChanged: (value) {
                   },
-                  decoration: new InputDecoration(
-                      hintText: 'Enter image url link'
-                  )
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                  color: Colors.pink[400],
-                  child: Text(
-                    'Customize location',
-                    style: TextStyle(color: Colors.white),
+                  decoration:  new InputDecoration(
+                      hintText: 'Choose tags'
                   ),
-                  onPressed: () {
-                    locationName = locName;
-                    imageURL = imgURL;
-                    if (locationName.isEmpty || imageURL.isEmpty) {
-                      setState(() => error = 'Please enter valid answers');
-                    } else {
-                      error = '';
-                      Navigator.pushNamed(context, '/home');
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                    onChanged: (val) {
+                      setState(() => imgURL = val);
+                    },
+                    decoration: new InputDecoration(
+                        hintText: 'Enter image url link'
+                    )
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  /*autovalidate: true,
+                  keyboardType: TextInputType.numberWithOptions(),
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    validator: (input) {
+                    final isDigitsOnly = double.tryParse(input);
+                    return isDigitsOnly == null ? 'Input needs to be digits only' : null;
+                    },*/
+                    onChanged: (value) => currLat = double.parse(value),
+                    decoration: new InputDecoration(
+                        hintText: 'Enter intended Latitude value'
+                    )
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                    /*autovalidate: true,
+                    keyboardType: TextInputType.numberWithOptions(),
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    validator: (input) {
+                      final isDigitsOnly = double.tryParse(input);
+                      return isDigitsOnly == null ? 'Input needs to be digits only' : null;
+                    },*/
+                    onChanged: (value) => currLong = double.parse(value),
+                    decoration: new InputDecoration(
+                        hintText: 'Enter intended Longitude value'
+                    )
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                    color: Colors.pink[400],
+                    child: Text(
+                      'Customize location',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      locationName = locName;
+                      imageURL = imgURL;
+                      if (locationName.isEmpty || imageURL.isEmpty) {
+                        setState(() => error = 'Please enter valid answers');
+                      } else {
+                        error = '';
+                        Navigator.pushNamed(context, '/home');
+                        addLocation();
+                      }
+
+
+                      print(locName);
+                      print(category);
+                      print(currentTags);
+                      print(imgURL);
+                      print(currLat.toString());
+                      print(currLong.toString());
                     }
-
-
-                    print(locName);
-                    print(category);
-                    print(currentTags);
-                    print(imgURL);
-
-                  }
-              ),
-               SizedBox(height: 12.0),
-               Text(
-                error,
-                 style: TextStyle(color: Colors.red, fontSize: 14.0),
-               ),
-            ],
+                ),
+                 SizedBox(height: 12.0),
+                 Text(
+                  error,
+                   style: TextStyle(color: Colors.red, fontSize: 14.0),
+                 ),
+              ],
+            ),
           ),
         ),
       ),
